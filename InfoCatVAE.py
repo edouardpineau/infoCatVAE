88d59c4e50afcea7d39792c43907facda6924299
edouardpineau <pineau.edouard@gmail.com>
2018-06-22 15:07:42 +0200

    Add files via upload

---
+++ InfoCatVAE.py
@@ -0,0 +1,64 @@
+# https://github.com/pytorch/examples/blob/master/vae/main.py
+
+from d592151.infoCatVAE.utils import *
+from safrantech.loaddata.MNIST import extract_mnist_data, extract_mnist_labels
+
+class InfoCatVAE(nn.Module):
+    def __init__(self, in_dim, num_class, sub_dim, z_dim, h_dim, lmbda, is_cuda):
+        super(InfoCatVAE, self).__init__()
+
+        self.in_dim = in_dim
+        self.num_class = num_class
+        self.sub_dim = sub_dim
+        self.z_dim = z_dim
+        self.h_dim = h_dim
+
+        self.lmbda = lmbda
+
+        self.fc1 = nn.Linear(in_dim, h_dim)
+        self.fc21 = nn.Linear(h_dim + num_class, z_dim)
+        self.fc22 = nn.Linear(h_dim + num_class, z_dim)
+        self.fca = nn.Linear(h_dim, num_class)
+        self.fc3 = nn.Linear(z_dim, h_dim)
+        self.fc4 = nn.Linear(h_dim, in_dim)
+
+        self.relu = nn.ReLU()
+        self.sigmoid = nn.Sigmoid()
+
+        self.is_cuda = is_cuda
+
+    def encode(self, x):
+        h1 = F.relu(F.dropout(self.fc1(x), p=0.25))
+        a = F.softmax(self.fca(h1), dim=1)
+        idt = torch.eye(self.num_class)
+
+        if self.is_cuda:
+            allmu = torch.stack([self.fc21(torch.cat((h1, Variable(idt[i, :].repeat(h1.size(0), 1)).cuda()), 1))
+                                 for i in range(self.num_class)])
+            allvar = torch.stack([self.fc22(torch.cat((h1, Variable(idt[i, :].repeat(h1.size(0), 1)).cuda()), 1))
+                                  for i in range(self.num_class)])
+        else:
+            allmu = torch.stack([self.fc21(torch.cat((h1, Variable(idt[i, :].repeat(h1.size(0), 1))), 1))
+                                 for i in range(self.num_class)])
+            allvar = torch.stack([self.fc22(torch.cat((h1, Variable(idt[i, :].repeat(h1.size(0), 1))), 1))
+                                  for i in range(self.num_class)])
+
+        return self.fc21(torch.cat((h1, a), 1)), self.fc22(torch.cat((h1, a), 1)), a, allmu, allvar
+
+    def reparameterize(self, mu, logvar):
+        if self.training:
+            std = logvar.mul(0.5).exp_()
+            eps = Variable(std.data.new(std.size()).normal_())
+            z = eps.mul(std).add_(mu)
+            return z
+        else:
+            return mu
+
+    def decode(self, z):
+        h3 = F.relu(F.dropout(self.fc3(z), p=0.25))
+        return self.sigmoid(self.fc4(h3))
+
+    def forward(self, x):
+        mu, logvar, a, allmu, allvar = self.encode(x.view(-1, self.in_dim))
+        z = self.reparameterize(mu, logvar)
+        return self.decode(z), mu, logvar, a, allmu, allvar
---
+++ utils.py
@@ -0,0 +1,72 @@
+from __future__ import print_function
+import argparse
+import torch
+import torch.utils.data
+from torch import nn, optim
+from torch.autograd import Variable
+from torch.nn import functional as F
+from torchvision.utils import save_image
+import numpy as np
+import pandas as pd
+
+
+
+def KL_gaussian(mu1, mu2, logvar):
+    return -0.5 * (1 + logvar - (mu1 - mu2).pow(2) - logvar.exp())
+
+
+def prior_construction(model):
+    num_class, sub_dim, lmbda, is_cuda = model.num_class, model.sub_dim, model.lmbda, model.is_cuda
+    muprior = np.zeros((model.num_class*model.sub_dim, model.num_class))
+
+    for i in range(model.num_class):
+        for j in range(model.sub_dim):
+            muprior[model.sub_dim * i + j, i] = model.lmbda
+
+    if model.is_cuda:
+        mupriorT = Variable(torch.from_numpy(muprior).type(torch.FloatTensor)).cuda()
+    else:
+        mupriorT = Variable(torch.from_numpy(muprior).type(torch.FloatTensor))
+
+    return mupriorT
+
+
+def loss_function(model, recon_x, x, a, allmu, allvar, mupriorT):
+    BCE = F.binary_cross_entropy(recon_x, x.view(-1, model.in_dim), size_average=False)
+    MSE = F.mse_loss(recon_x, x.view(-1, model.in_dim))
+
+    KLD = 0
+    for i in range(model.num_class):
+        KLD += torch.sum(torch.t(a[:, i].repeat(model.z_dim, 1)) * KL_gaussian(allmu[i, :, :],
+                                                                               mupriorT[:, i].repeat(allmu.size(1), 1),
+                                                                               allvar[i, :, :]))
+
+    negH = torch.sum(a.mul(torch.log(a + 0.001)))
+
+    return BCE + 100*negH + 100*KLD, BCE, negH, KLD, MSE
+
+
+def sampling(model, k, mupriorT):
+    allz = []
+    if model.is_cuda:
+        for i in range(k):
+            mu = mupriorT.t().data
+            logvar = torch.FloatTensor(np.zeros((model.num_class, model.z_dim)))
+            z = model.reparameterize(Variable(mu).cuda(), Variable(logvar).cuda())
+            allz.append(z)
+        sample = torch.cat(allz, dim=0)  # Variable(torch.randn(64, 20))
+        sample = model.decode(sample)
+    else:
+        for i in range(k):
+            mu = mupriorT.t().data
+            logvar = torch.FloatTensor(np.zeros((model.num_class, model.z_dim)))
+            z = model.reparameterize(Variable(mu), Variable(logvar))
+            allz.append(z)
+        sample = torch.cat(allz, dim=0)  # Variable(torch.randn(64, 20))
+        sample = model.decode(sample)
+
+    return sample, Variable(torch.arange(0, model.num_class).repeat(k).long())
+
+
+
+
