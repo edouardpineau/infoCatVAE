74bada46b1f051a4129c4165fcf0f29aebea6e52
edouardpineau <pineau.edouard@gmail.com>
2018-06-22 09:46:33 +0000

    Update infoCatConvVAE.py

--- infoCatConvVAE.py
+++ infoCatConvVAE.py
@@ -1,13 +1,4 @@
-from __future__ import print_function
-import argparse
-import torch
-import torch.utils.data
-from torch import nn, optim
-from torch.autograd import Variable
-from torch.nn import functional as F
-from torchvision.utils import save_image
-import numpy as np
-import pandas as pd
+from utils import *
 
 class InfoCatConvVAE(nn.Module):
     def __init__(self, in_dim, num_class, sub_dim, z_dim, h_dim):
