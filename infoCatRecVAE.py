5af9dd8f2fe71026beb38f4447c98bf90f0753a2
edouardpineau <pineau.edouard@gmail.com>
2018-06-22 09:46:51 +0000

    Update infoCatRecVAE.py

--- infoCatRecVAE.py
+++ infoCatRecVAE.py
@@ -1,13 +1,5 @@
-from __future__ import print_function
-import argparse
-import torch
-import torch.utils.data
-from torch import nn, optim
-from torch.autograd import Variable
-from torch.nn import functional as F
-from torchvision.utils import save_image
-import numpy as np
-import pandas as pd
+from utils import *
+
 
 class InfoCatRVAE(nn.Module):
     def __init__(self, in_dim, h_dim, z_dim, num_class, n_layers, bidirectional_encoder=True):
