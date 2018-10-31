a98d0ca7e1c4fddd8666dd63a6e4faf2ecc9f81f
edouardpineau <pineau.edouard@gmail.com>
2018-10-31 11:02:48 +0100

    Update InfoCatVAE.py

--- InfoCatVAE.py
+++ InfoCatVAE.py
@@ -1,6 +1,3 @@
-# https://github.com/pytorch/examples/blob/master/vae/main.py
-
-from utils import *
 
 class InfoCatVAE(nn.Module):
     def __init__(self, in_dim, num_class, sub_dim, z_dim, h_dim, lmbda, is_cuda):
@@ -13,7 +10,7 @@ class InfoCatVAE(nn.Module):
         :param lmbda: subspace mean axis values
         :param is_cuda: instruction about the cuda encoding of the Pytorch variables
         """
-        
+
         super(InfoCatVAE, self).__init__()
 
         self.in_dim = in_dim
@@ -24,7 +21,7 @@ class InfoCatVAE(nn.Module):
 
         self.lmbda = lmbda
 
-        self.fc1 = nn.Linear(in_dim, h_dim)
+        self.fc1 = nn.Sequential(nn.Linear(in_dim, h_dim))
         self.fc21 = nn.Linear(h_dim + num_class, z_dim)
         self.fc22 = nn.Linear(h_dim + num_class, z_dim)
         self.fca = nn.Linear(h_dim, num_class)
@@ -36,6 +33,8 @@ class InfoCatVAE(nn.Module):
 
         self.is_cuda = is_cuda
 
+        self.mupriorT = None
+
     def encode(self, x):
         """
         Encodes the input x into a subspace clustering type of mixture distribution
@@ -43,7 +42,7 @@ class InfoCatVAE(nn.Module):
         :return: parameters of the latent distribution for the specific input associated with each latent category
                  + probabilities of being in each category
         """
-        h1 = F.relu(F.dropout(self.fc1(x), p=0.25))
+        h1 = F.dropout(F.relu(self.fc1(x)), p=0.25)
         a = F.softmax(self.fca(h1), dim=1)
         idt = torch.eye(self.num_class)
 
@@ -63,12 +62,12 @@ class InfoCatVAE(nn.Module):
     def reparameterize(self, mu, logvar):
         """
         Model the reparametrization trick for gaussian sampling back-propagation
-        
+
         :param mu: mean of the gaussian
         :param logvar: logvariance of the gaussian
         :return: samples from the gaussian
         """
-        
+
         if self.training:
             std = logvar.mul(0.5).exp_()
             eps = Variable(std.data.new(std.size()).normal_())
@@ -77,13 +76,32 @@ class InfoCatVAE(nn.Module):
         else:
             return mu
 
+    def gumbel_softmax(self, logits, temperature=1.0, eps=1e-9):
+        '''
+        :param logits: shape: N*L
+        :param temperature:
+        :param eps:
+        :return:
+        '''
+        # get gumbel noise
+        noise = torch.rand(logits.size())
+        noise.add_(eps).log_().neg_()
+        noise.add_(eps).log_().neg_()
+        noise = Variable(noise)
+        if self.is_cuda:
+            noise = noise.cuda()
+
+        x = (logits + noise) / temperature
+        x = F.softmax(x, dim=1)
+        return x
+
     def decode(self, z):
         """
         MLP decoder
         :param z: latent sample before reconstruction / generation
         :return: reconstruction / generation
         """
-        
+
         h3 = F.relu(F.dropout(self.fc3(z), p=0.25))
         return self.sigmoid(self.fc4(h3))
 
@@ -93,7 +111,7 @@ class InfoCatVAE(nn.Module):
         :param x: total mixture autoencoder process
         :return: reconstruction + latent parameters
         """
-        
+
         mu, logvar, a, allmu, allvar = self.encode(x.view(-1, self.in_dim))
         z = self.reparameterize(mu, logvar)
         return self.decode(z), mu, logvar, a, allmu, allvar
